fb32d33d2e7b777fe24af5661146d3ac290864b5
EasterFan <fan.easter@gmail.com>
2020-06-10 16:19:44 +0800

    finish header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { WiredButton, WiredInput, WiredTextarea } from "wired-elements"
 import './Header.css';
 
 function Header() {
   return (
     <div className="Header">
-      <h1>This website will self destruct</h1>
+      <h1>This website will self destruct - 遗愿清单</h1>
     </div>
   );
 }
