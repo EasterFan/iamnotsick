37176e86c02ec32f028391c275e55e646dcc3fc7
EasterFan <fan.easter@gmail.com>
2020-06-15 00:10:12 +0800

    read a letter

--- src/components/Mod.js
+++ src/components/Mod.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { WiredButton, WiredCard, WiredFab } from "wired-elements"
 import './Mod.css';
+import  axios from 'axios';
 
-function Mod() {
-  return (
-    <div className="mod">
-      <wired-card elevation="3">
-        <wired-button> Read A Message </wired-button>
+class Mod extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: "",
+      isClicked: false
+    };
+    this.readLetter = this.readLetter.bind(this);
+  }
 
-        <wired-fab className="red">
-          <mwc-icon></mwc-icon>
-        </wired-fab>
+readLetter(event){
 
-        <wired-button> Feeling Down? </wired-button>
-      </wired-card>
+let self = this;
+axios.get('/letter')
+  .then(function (response) {
+    self.setState({message:response.data.message});
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+  });
 
-    </div>
+}
 
-  );
+  render() {
+    return (
+      <div className="mod">
+        <wired-card id="read" elevation="3">
+          {this.state.message}
+        </wired-card>
+
+        <wired-card elevation="3">
+          <wired-button onClick={this.readLetter}> Read A Message </wired-button>
+
+          <wired-fab className="red">
+            <mwc-icon></mwc-icon>
+          </wired-fab>
+
+          <wired-button> Feeling Down? </wired-button>
+        </wired-card>
+
+      </div>
+    );
+  }
 }
 
 export default Mod;
